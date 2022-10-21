250e1076b55fb0d1dc842997e17afc1d6be45956
simon <brooklinsimon29@gmail.com>
2022-10-21 22:34:00 +0530

    bass-app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'BASS-JS_v2'`, () => {
+  it(`should have as title 'BASS-JS'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('BASS-JS_v2');
+    expect(app.title).toEqual('BASS-JS');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('BASS-JS_v2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('BASS-JS app is running!');
   });
 });
